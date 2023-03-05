818ce382e329cd30d3270374f30b62b9bf6c30e1
asksnowflake <127050303+asksnowflake@users.noreply.github.com>
2023-03-05 14:33:16 -0500

    Update streamlit_app.py
    added few lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('1. Poha')
+streamlit.text('2. Idli')
+streamlit.text('3. Wada')
+streamlit.text('4. Misal')
